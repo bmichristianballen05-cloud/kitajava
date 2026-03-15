bbf110ee51c7689f6a31c25f4510e4802f6b918d
bmichristianballen05-cloud <bmi.christianballen05@gmail.com>
2026-03-15 21:13:20 +0800

    Add files via upload

---
+++ kitajava/src/DBConnection.java
@@ -0,0 +1,15 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// DBConnection.java
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3307/kita_system?";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
---
+++ kitajava/src/jPanel4.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Precious
+ */
+class jPanel4 {
+
+    static void revalidate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void add(JPanel card) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void repaint() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
